a117ce973cfedab92f4e1a2e9e416b80397a0968
Ventura Junior <you@example.com>
2023-07-06 12:07:11 -0300

    Sidebar compoment styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { Post } from "./Post"
 import { Header } from "./components/Header"
 
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Ventura Junior"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio possimus, enim asperiores quia similique reprehenderit, ratione ullam ipsum nemo sed repellendus, modi itaque cumque magni. Quasi eum omnis doloremque officiis."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ventura Junior"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio possimus, enim asperiores quia similique reprehenderit, ratione ullam ipsum nemo sed repellendus, modi itaque cumque magni. Quasi eum omnis doloremque officiis."
+          />
+        </main>
+      </div>
     </>
   )
 }
